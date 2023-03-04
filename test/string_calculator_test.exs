fe36558489c71a94e9673000f09b4030d51e5d2e
birchturtle <kj@birchturtle.net>
2023-03-04 13:24:21 +0100

    add extra test for 9 nos

--- test/string_calculator_test.exs
+++ test/string_calculator_test.exs
@@ -1,10 +1,14 @@
 defmodule StringCalculatorTest do
   use ExUnit.Case
   doctest StringCalculator
+  
   test "can add 2 numbers" do
     assert StringCalculator.add("1,2") == 3
   end
   test "can add 3 numbers" do
     assert StringCalculator.add("1,2,3") == 6
+  end
+  test "can add 9 numbers" do
+    assert StringCalculator.add("3,3,3,3,3,3,3,3,3") === 27
   end
 end
