fdd1d374f85d71a40eed002eabfed641d2f74c99
birchturtle <kj@birchturtle.net>
2023-03-04 13:19:47 +0100

    add test for 3 numbers

--- test/string_calculator_test.exs
+++ test/string_calculator_test.exs
@@ -1,11 +1,10 @@
 defmodule StringCalculatorTest do
   use ExUnit.Case
   doctest StringCalculator
-
-  test "greets the world" do
-    assert StringCalculator.hello() == :world
-  end
   test "can add 2 numbers" do
     assert StringCalculator.add("1,2") == 3
+  end
+  test "can add 3 numbers" do
+    assert StringCalculator.add("1,2,3") == 6
   end
 end
