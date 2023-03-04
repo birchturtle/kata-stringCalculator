902f4a66e32953d29f797e7f2e2b3f744475ff3e
birchturtle <kj@birchturtle.net>
2023-03-04 13:40:16 +0100

    add newline as allowed delimiter

--- lib/string_calculator.ex
+++ lib/string_calculator.ex
@@ -2,21 +2,13 @@ defmodule StringCalculator do
   @moduledoc """
   Documentation for `StringCalculator`.
   """
-
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> StringCalculator.hello()
-      :world
-
   """
-  def hello do
-    :world
-  end
 
   def add(numbers) do
-    Enum.reduce(String.split(numbers, ","), 0, fn(n, acc) -> String.to_integer(n) + acc end)
+    Enum.reduce(split_string(numbers), 0, fn(n, acc) -> String.to_integer(n) + acc end)
+  end
+  defp split_string(numberstring) do
+    Regex.split(~r/[,\n]/, numberstring)
   end
 end
--- test/string_calculator_test.exs
+++ test/string_calculator_test.exs
@@ -1,14 +1,22 @@
 defmodule StringCalculatorTest do
   use ExUnit.Case
   doctest StringCalculator
-  
-  test "can add 2 numbers" do
-    assert StringCalculator.add("1,2") == 3
+
+  describe "basic requirements" do    
+    test "can add 2 numbers" do
+      assert StringCalculator.add("1,2") == 3
+    end
+    test "can add 3 numbers" do
+      assert StringCalculator.add("1,2,3") == 6
+    end
+   test "can add 9 numbers" do
+      assert StringCalculator.add("3,3,3,3,3,3,3,3,3") === 27
+    end
   end
-  test "can add 3 numbers" do
-    assert StringCalculator.add("1,2,3") == 6
-  end
-  test "can add 9 numbers" do
-    assert StringCalculator.add("3,3,3,3,3,3,3,3,3") === 27
+
+  describe "'advanced' requirements" do
+    test "it allows newlines as delimiters" do
+      assert StringCalculator.add("1\n2,3") == 6
+    end
   end
 end
